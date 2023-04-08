c1e6c5d3fa3c4d0d7f182db8b64a1ab5d136f62b
Anton Smirnov <DarkLightWindraK@yandex.ru>
2023-04-08 22:44:18 +0300

    Initial Commit

---
+++ Timetable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timetable
+//
+//  Created by Антон Смирнов on 08.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
